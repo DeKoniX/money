e11f37abeddcd9f8ee7b0955d9f6a7abc7cb51f6
DeKoniX <dekonix@gmail.com>
2013-02-04 21:31:53 +1000

    Работааает, первая моя аундефикация

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116220154) do
+ActiveRecord::Schema.define(:version => 20130203163248) do
 
   create_table "finances", :force => true do |t|
-    t.integer  "m"
+    t.decimal  "m"
     t.text     "desc"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(:version => 20130116220154) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
