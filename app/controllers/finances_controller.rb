47c9750f42f979007f3f80a0a30b52def892b9c6
DeKoniX <dekonix@gmail.com>
2013-02-05 12:41:14 +1000

    Красавости, общее значение денег, точка

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,2 +1,28 @@
 class FinancesController < InheritedResources::Base
+  before_filter :signed_in_user
+
+  def create
+    @finance = current_user.finance.build(params[:finance])
+    if @finance.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @money = 0
+    current_user.finance.each do |fin|
+      @money += fin.m
+    end
+  end
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 end
--- db/migrate/20130103172831_create_finances.rb
+++ db/migrate/20130103172831_create_finances.rb
@@ -1,7 +1,7 @@
 class CreateFinances < ActiveRecord::Migration
   def change
     create_table :finances do |t|
-      t.decimal :m
+      t.decimal :m, scale: 2
       t.text :desc
 
       t.timestamps
