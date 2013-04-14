4b8d6973c08bab4d340ab846d604b7e327b92390
DeKoniX <dekonix@DeKoniX-Laptop.(none)>
2013-04-15 04:09:44 +0700

    При добавлении должника можно сразу указать его долг

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,23 +1,33 @@
 class PeopleController < InheritedResources::Base
 
-	before_filter :corrent_user, only: [:edit, :update, :destroy]
+  before_filter :corrent_user, only: [:edit, :update, :destroy]
 
-	def create
-		create! { people_path }
-		@person.user_id = current_user.id
-		@person.save!
-	end
-	def update
-		update! { people_path }
-	end
-	def destroy
-		destroy! { people_path }
-	end	
+  def new
+    @debts = Debt.new
+    new!
+  end
 
-	private
-		def corrent_user
-			if current_user.id != Person.find(params[:id]).user_id
-	      redirect_to root_url
-	    end
-	  end
+  def create
+    create! { people_path }
+    @person.user_id = current_user.id
+    @person.save!
+    @debt = Debt.new(params[:debt])
+    @debt.person_id = @person.id
+    @debt.save!
+  end
+
+  def update
+    update! { people_path }
+  end
+
+  def destroy
+    destroy! { people_path }
+  end
+
+  private
+  def corrent_user
+    if current_user.id != Person.find(params[:id]).user_id
+      redirect_to root_url
+    end
+  end
 end
